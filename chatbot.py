ab52d45cb5ef0c59769857920583286b02870967
paulinho9986 <paulo_henrique5002@hotmail.com>
2025-05-30 23:39:16 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -74,7 +74,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 # CONSTANTES E CONFIGURAÇÕES
 # ======================
 class Config:
-    API_KEY = "AIzaSyDTaYm2KHHnVPdWy4l5pEaGPM7QR0g3IPc"
+    API_KEY = "AIzaSyClo6IrwckwA5hrZbWq6MOZmtT8PqAZsbc"
     API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}"
     VIP_LINK = "https://exemplo.com/vip"
     CHECKOUT_START = "https://checkout.exemplo.com/start"
